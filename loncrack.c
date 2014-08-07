7756638d353fd5e86284faacda950321a5936392
ezekmoraes <ezekkkiel@outlook.com>
2014-08-07 10:51:39 -0300

    Update loncrack.c

--- loncrack.c
+++ loncrack.c
@@ -10,28 +10,35 @@ int main(int argc, char *argv[])
 
 	char senha[75];
 	char salt[25];
-	char comp[100];
+	char comp[100]; // hash
 	char *result;
 
-	printf("Digite o Hash completo\n");
+	printf("Digite o Hash completo: ");
 	scanf("%s", comp);
 
-	printf("Digite o Salt\n");
+	printf("Digite o Salt: ");
 	scanf("%s",salt);
 	
 	int f = 0;
 
 	while(fscanf(arq, "%s", &senha) != EOF)
 	{
-	result = (char *) crypt(senha, salt);
-		if (strcmp(comp,result)==0){
-		printf("Senha encontrada: %s \n", senha);
-		int f = 1;
-		return(0);
-		}else{
-		printf("Testando.. %s \n", senha);
+		result = (char *) crypt(senha, salt);
+		
+		if (strcmp(comp,result) == 0)
+		{
+			printf("Senha encontrada: %s \n", senha);
+			int f = 1;
+			return(0);
 		}
-	}if(f==0){
-	printf("Senha não encontrada..\n");
+		else
+		{
+			printf("Testando.. %s \n", senha);
+		}
+	}
+	// se a senha não for encontrada
+	if(f==0)
+	{
+		printf("Senha não encontrada..\n");
 	}
 }
